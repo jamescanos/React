04d09caa1a21b28391811a58fb14289b4d25e8df
Jhon James Cano <jhonjamescanosanchez@gmail.com>
2022-09-19 09:12:21 -0500

    First Functional Components

---
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,18 @@
+/*function App(){
+    return (<h1>Hola Mundo !!!</h1>);
+}*/
+
+/*function App(){
+    return (<h1>Hola Mundo !!!</h1>);
+}
+
+export default App;*/
+
+// Functional Component => Basado en una función de flecha
+export const HelloWorldApp = () => {
+
+    return(
+        <h1>Hello World App</h1>
+    );
+
+}
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// Se usa cuando es un default
+//import App from './HelloWorldApp';
+import { HelloWorldApp } from './HelloWorldApp';
+import { FirstApp } from './FirstApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <React.StrictMode>
+        <HelloWorldApp/> 
+        <FirstApp/> 
+    </React.StrictMode>
+);
