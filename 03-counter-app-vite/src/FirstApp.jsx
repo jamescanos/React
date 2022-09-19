50fcfaa3f264636d069ffb19f70b36e8810bd4f7
Jhon James Cano <jhonjamescanosanchez@gmail.com>
2022-09-19 17:21:34 -0500

    React Properties (Prop)

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,7 +1,38 @@
-export const FirstApp = () => {
+/*import { Fragment } from "react";
 
+export const FirstApp = () => {
+    <Fragment>
     return(
         <h1>FirstApp</h1>
     );
+    </Fragment>
+
+}*/
+
+const myFunction = (a, b) => {
+
+    return a+b;
+
+}
+
+const newMessage = {
+
+    message: "Hola",
+    title: "JJ"
+
+}
+
+export const FirstApp = ({ title, valor }) => {
+    // <> Es un Fragment sin necesidad de importar    
+    return(
+        <>
+            
+            <h1>{ /*newMessage.message + " " +newMessage.title*/ myFunction(2,3)   }</h1>
+            <h2>{ title }</h2>
+            <h3>{ valor }</h3>
+            <p>Nuevo Elemento</p>
+        </>
+    );
+    
 
 }
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 
 // Se usa cuando es un default
 //import App from './HelloWorldApp';
-import { HelloWorldApp } from './HelloWorldApp';
+//import { HelloWorldApp } from './HelloWorldApp';
 import { FirstApp } from './FirstApp';
+
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <HelloWorldApp/> 
-        <FirstApp/> 
+        <FirstApp title = "Creando Properties (Prop)" valor = {2022} /> 
     </React.StrictMode>
 );
